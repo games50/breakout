7f1704ef5d9780578261da29e972e0389b43ac8e
Colton Ogden <coltonoscopy@gmail.com>
2018-02-06 12:20:36 -0500

    breakout3 and 4

---
+++ breakout3/src/states/PlayState.lua
@@ -0,0 +1,91 @@
+--[[
+    GD50
+    Breakout Remake
+
+    -- PlayState Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Represents the state of the game in which we are actively playing;
+    player should control the paddle, with the ball actively bouncing between
+    the bricks, walls, and the paddle. If the ball goes below the paddle, then
+    the player should lose one point of health and be taken either to the Game
+    Over screen if at 0 health or the Serve screen otherwise.
+]]
+
+PlayState = Class{__includes = BaseState}
+
+function PlayState:init()
+    self.paddle = Paddle()
+
+    -- initialize ball with skin #1; different skins = different sprites
+    self.ball = Ball(1)
+
+    -- give ball random starting velocity
+    self.ball.dx = math.random(-200, 200)
+    self.ball.dy = math.random(-50, -60)
+
+    -- give ball position in the center
+    self.ball.x = VIRTUAL_WIDTH / 2 - 4
+    self.ball.y = VIRTUAL_HEIGHT - 42
+
+    -- use the "static" createMap function to generate a bricks table
+    self.bricks = LevelMaker.createMap()
+end
+
+function PlayState:update(dt)
+    if self.paused then
+        if love.keyboard.wasPressed('space') then
+            self.paused = false
+            gSounds['pause']:play()
+        else
+            return
+        end
+    elseif love.keyboard.wasPressed('space') then
+        self.paused = true
+        gSounds['pause']:play()
+        return
+    end
+
+    -- update positions based on velocity
+    self.paddle:update(dt)
+    self.ball:update(dt)
+
+    if self.ball:collides(self.paddle) then
+        -- reverse Y velocity if collision detected between paddle and ball
+        self.ball.dy = -self.ball.dy
+        gSounds['paddle-hit']:play()
+    end
+
+    -- detect collision across all bricks with the ball
+    for k, brick in pairs(self.bricks) do
+
+        -- only check collision if we're in play
+        if brick.inPlay and self.ball:collides(brick) then
+
+            -- trigger the brick's hit function, which removes it from play
+            brick:hit()
+        end
+    end
+
+    if love.keyboard.wasPressed('escape') then
+        love.event.quit()
+    end
+end
+
+function PlayState:render()
+    -- render bricks
+    for k, brick in pairs(self.bricks) do
+        brick:render()
+    end
+
+    self.paddle:render()
+    self.ball:render()
+
+    -- pause text, if paused
+    if self.paused then
+        love.graphics.setFont(largeFont)
+        love.graphics.printf("PAUSED", 0, VIRTUAL_HEIGHT / 2 - 16, VIRTUAL_WIDTH, 'center')
+    end
+end
---
+++ breakout3/src/states/StartState.lua
@@ -0,0 +1,73 @@
+--[[
+    GD50
+    Breakout Remake
+
+    -- StartState Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Represents the state the game is in when we've just started; should
+    simply display "Breakout" in large text, as well as a message to press
+    Enter to begin.
+]]
+
+-- the "__includes" bit here means we're going to inherit all of the methods
+-- that BaseState has, so it will have empty versions of all StateMachine methods
+-- even if we don't override them ourselves; handy to avoid superfluous code!
+StartState = Class{__includes = BaseState}
+
+-- whether we're highlighting "Start" or "High Scores"
+local highlighted = 1
+
+function StartState:update(dt)
+    -- toggle highlighted option if we press an arrow key up or down
+    if love.keyboard.wasPressed('up') or love.keyboard.wasPressed('down') then
+        highlighted = highlighted == 1 and 2 or 1
+        gSounds['paddle-hit']:play()
+    end
+
+    -- confirm whichever option we have selected to change screens
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        gSounds['confirm']:play()
+
+        if highlighted == 1 then
+            gStateMachine:change('play')
+        end
+    end
+
+    -- we no longer have this globally, so include here
+    if love.keyboard.wasPressed('escape') then
+        love.event.quit()
+    end
+end
+
+function StartState:render()
+    -- title
+    love.graphics.setFont(gFonts['large'])
+    love.graphics.printf("BREAKOUT", 0, VIRTUAL_HEIGHT / 3,
+        VIRTUAL_WIDTH, 'center')
+    
+    -- instructions
+    love.graphics.setFont(gFonts['medium'])
+
+    -- if we're highlighting 1, render that option blue
+    if highlighted == 1 then
+        love.graphics.setColor(103, 255, 255, 255)
+    end
+    love.graphics.printf("START", 0, VIRTUAL_HEIGHT / 2 + 70,
+        VIRTUAL_WIDTH, 'center')
+
+    -- reset the color
+    love.graphics.setColor(255, 255, 255, 255)
+
+    -- render option 2 blue if we're highlighting that one
+    if highlighted == 2 then
+        love.graphics.setColor(103, 255, 255, 255)
+    end
+    love.graphics.printf("HIGH SCORES", 0, VIRTUAL_HEIGHT / 2 + 90,
+        VIRTUAL_WIDTH, 'center')
+
+    -- reset the color
+    love.graphics.setColor(255, 255, 255, 255)
+end
